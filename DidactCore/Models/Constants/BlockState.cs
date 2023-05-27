53d759a26177c386e40fa059afd2b849b258146d
Daniel Miradakis <dmiradakis@owensgrp.com>
2023-05-27 17:10:07 -0500

    Added BlockState class

---
+++ DidactCore/Models/Constants/BlockState.cs
@@ -0,0 +1,13 @@
+﻿namespace DidactCore.Models.Constants
+{
+    public static class BlockState
+    {
+        public const string Idle = "Idle";
+        public const string Running = "Running";
+        public const string Retrying = "Retrying";
+        public const string Failing = "Failing";
+        public const string Failed = "Failed";
+        public const string Succeeded = "Succeeded";
+        public const string Cancelled = "Cancelled";
+    }
+}
