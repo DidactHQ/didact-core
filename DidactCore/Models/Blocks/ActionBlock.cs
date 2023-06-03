117025b9248f140ecae958cce54b96c772de1c01
Daniel Miradakis <dmiradakis@owensgrp.com>
2023-06-03 00:35:31 -0500

    Major timeout enhacements to Action Block. Introduced soft timeout.

--- DidactCore/Models/Blocks/ActionBlock.cs
+++ DidactCore/Models/Blocks/ActionBlock.cs
@@ -25,13 +25,13 @@ namespace DidactCore.Models.Blocks
 
         public int RetryDelayMilliseconds { get; set; }
 
-        public int Timeout { get; set; }
+        public int SoftTimeout { get; set; }
 
         public int RetriesAttempted { get; private set; } = 0;
 
         public int RuntimeMinutesElapsed { get; private set; }
 
-        public bool TimeoutExceeded { get; private set; }
+        public bool SoftTimeoutExceeded { get; private set; }
 
 
         public ActionBlock(ILogger logger, IBlockRepository blockRepository)
@@ -40,12 +40,22 @@ namespace DidactCore.Models.Blocks
             _blockRepository = blockRepository;
         }
 
+        /// <summary>
+        /// Sets the delegate to execute. Since this is a delegate, it can be many things, like an expression tree that you define in this constructor or a method from somewhere else.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
         public ActionBlock<T> WithExecutor(Action<T> action)
         {
             Action = action;
             return this;
         }
 
+        /// <summary>
+        /// Sets the arguments for the delegate.
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
         public ActionBlock<T> WithArgs(T param)
         {
             Parameter = param;
@@ -58,9 +68,15 @@ namespace DidactCore.Models.Blocks
             return this;
         }
 
-        public ActionBlock<T> WithTimeout(int timeoutThreshold)
+        /// <summary>
+        /// Sets the soft timeout threshold for the block.
+        /// The soft timeout will NOT abort the delegate's execution, but it will check for a violation before and after the delegate's execution and retries.
+        /// </summary>
+        /// <param name="softTimeoutThreshold"></param>
+        /// <returns></returns>
+        public ActionBlock<T> WithSoftTimeout(int softTimeoutThreshold)
         {
-            Timeout = timeoutThreshold;
+            SoftTimeout = softTimeoutThreshold;
             return this;
         }
 
@@ -71,7 +87,7 @@ namespace DidactCore.Models.Blocks
             return this;
         }
 
-        public async Task ExecuteDelegateAsync()
+        private async Task ExecuteDelegateAsync()
         {
             if (Action == null || Parameter == null)
             {
@@ -80,12 +96,19 @@ namespace DidactCore.Models.Blocks
 
             while (RetriesAttempted <= RetryAttemptsThreshold)
             {
+                if (SoftTimeoutExceeded)
+                {
+                    _logger.LogInformation("The soft timeout threshold has been exceeded. Cancelling execution...");
+                    break;
+                }
+
                 try
                 {
                     State = BlockState.Running;
                     _logger.LogInformation("Action Block {name} executing delegate...", Name);
                     Action(Parameter);
                     State = BlockState.Succeeded;
+                    break;
                 }
                 catch (Exception ex)
                 {
@@ -93,18 +116,31 @@ namespace DidactCore.Models.Blocks
 
                     if (RetriesAttempted <= RetryAttemptsThreshold)
                     {
+                        if (SoftTimeoutExceeded)
+                        {
+                            _logger.LogInformation("The soft timeout threshold has been exceeded. Cancelling execution...");
+                            break;
+                        }
+
                         State = BlockState.Failing;
-                        _logger.LogError(ex, "Action Block {name} encountered an unhandled exception. See details: {ex}", Name, JsonSerializer.Serialize(ex, new JsonSerializerOptions(JsonSerializerDefaults.Web)));
+                        _logger.LogError("Action Block {name} encountered an unhandled exception. See details: {ex}", Name, ex.Message);
                         _logger.LogInformation("Action Block {name} awaiting retry delay...", Name);
                         await Task.Delay(RetryDelayMilliseconds);
                         State = BlockState.Retrying;
                         _logger.LogInformation("Action Block {name} attempting retry...", Name);
+
+                        if (SoftTimeoutExceeded)
+                        {
+                            _logger.LogInformation("The soft timeout threshold has been exceeded. Cancelling execution...");
+                            break;
+                        }
+
                         continue;
                     }
                     else
                     {
                         State = BlockState.Failed;
-                        _logger.LogCritical(ex, "Action Block {name} has encountered an unhandled exception and has now failed. See details: {ex}", Name, JsonSerializer.Serialize(ex, new JsonSerializerOptions(JsonSerializerDefaults.Web)));
+                        _logger.LogCritical("Action Block {name} has encountered an unhandled exception and has now failed. See details: {ex}", Name, ex.Message);
                         throw;
                     }
                 }
@@ -113,14 +149,14 @@ namespace DidactCore.Models.Blocks
 
         public async Task ExecuteAsync()
         {
-            var timeoutTask = Task.Delay(Timeout);
+            var timeoutTask = Task.Delay(SoftTimeout);
             var delegateTask = ExecuteDelegateAsync();
 
             if (timeoutTask == await Task.WhenAny(delegateTask, timeoutTask))
             {
                 State = BlockState.Failed;
-                _logger.LogCritical("Action Block {name} exceeded its timeout threshold.", Name);
-                throw new TimeoutException($"Action Block {Name} exceeded its timeout threshold.");
+                _logger.LogCritical("Action Block {name} exceeded its soft timeout threshold. Marking for cancellation...", Name);
+                SoftTimeoutExceeded = true;
             }
 
             await delegateTask;
