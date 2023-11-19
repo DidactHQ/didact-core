3e0873fd3f98935865b5d6a946dfa453585696ba
Daniel Miradakis <dmiradakis@owensgrp.com>
2023-11-19 01:23:25 -0600

    FlowConfigurator wip

--- DidactCore/Models/Flows/FlowConfigurator.cs
+++ DidactCore/Models/Flows/FlowConfigurator.cs
@@ -2,5 +2,30 @@
 {
     public class FlowConfigurator
     {
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        /// <summary>
+        /// Sets the Flow name.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public FlowConfigurator WithName(string name)
+        {
+            Name = name;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the Flow description.
+        /// </summary>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        public FlowConfigurator WithDescription(string description)
+        {
+            Description = description;
+            return this;
+        }
     }
 }
