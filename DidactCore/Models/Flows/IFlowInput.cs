7f63dd10ecf3972228d8a4f573e9fe2146c3ad39
Daniel Miradakis <dmiradakis@owensgrp.com>
2023-11-19 00:26:58 -0600

    Create IFlowInput interface

---
+++ DidactCore/Models/Flows/IFlowInput.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace DidactCore.Models.Flows
+{
+    public interface IFlowInput
+    {
+        /// <summary>
+        /// <para>Asynchronously configures the inputs of the Flow.</para>
+        /// <para>If no asynchronous actions occur in this method, simply end the method with Task.CompletedTask.</para>
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <returns></returns>
+        Task ConfigureInputsAsync(object[] inputs);
+    }
+}
