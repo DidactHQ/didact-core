8faf6cacbfa63743b7a068fe475ca31f838f2143
Daniel Miradakis <dmiradakis@owensgrp.com>
2023-11-20 00:21:23 -0600

    Removed xml markup from FlowConfigurator class

--- DidactCore/Models/Flows/FlowConfigurator.cs
+++ DidactCore/Models/Flows/FlowConfigurator.cs
@@ -6,10 +6,6 @@ using System.Threading.Tasks;
 
 namespace DidactCore.Models.Flows
 {
-    /// <summary>
-    /// <para>A helper class that configures important metadata for a Flow.</para>
-    /// <para>This class needs to be registered as a transient dependency in Didact Engine.</para>
-    /// </summary>
     public class FlowConfigurator : IFlowConfigurator
     {
         private readonly ILogger _logger;
@@ -25,40 +21,25 @@ namespace DidactCore.Models.Flows
             _flowRepository = flowRepository;
         }
 
-        /// <summary>
-        /// Sets the Flow name.
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
         public IFlowConfigurator WithName(string name)
         {
             Name = name;
             return this;
         }
 
-        /// <summary>
-        /// Sets the Flow description.
-        /// </summary>
-        /// <param name="description"></param>
-        /// <returns></returns>
         public IFlowConfigurator WithDescription(string description)
         {
             Description = description;
             return this;
         }
 
-        /// <summary>
-        /// Asynchronously saves the Flow configurations to persistent storage.
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="SaveFlowConfigurationsException"></exception>
         public async Task SaveConfigurationsAsync()
         {
             try
             {
                 _logger.LogInformation("Saving the Flow configurations to persistent storage...");
                 await _flowRepository.SaveConfigurationsAsync(this).ConfigureAwait(false);
-                _logger.LogInformation("Flow configurations saved successfully");
+                _logger.LogInformation("Flow configurations saved successfully.");
             }
             catch (Exception ex)
             {
