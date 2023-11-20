990ae07cccaf2f63c5476be1a21c127106b567bc
Daniel Miradakis <dmiradakis@owensgrp.com>
2023-11-20 00:26:10 -0600

    Added xml markup to IFlowRepository

--- DidactCore/Repositories/IFlowRepository.cs
+++ DidactCore/Repositories/IFlowRepository.cs
@@ -5,6 +5,12 @@ namespace DidactCore.Repositories
 {
     public interface IFlowRepository
     {
+        /// <summary>
+        /// Asynchronously saves the values from IFlowConfigurator to persistent storage.
+        /// </summary>
+        /// <param name="flowConfigurator"></param>
+        /// <see cref="IFlowConfigurator"/>
+        /// <returns></returns>
         Task SaveConfigurationsAsync(IFlowConfigurator flowConfigurator);
     }
 }
