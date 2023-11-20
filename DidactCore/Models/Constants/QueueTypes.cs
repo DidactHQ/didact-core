3604a3cade2f9b51ea62bbd85ffe0618643208ea
Daniel Miradakis <dmiradakis@owensgrp.com>
2023-11-20 01:55:44 -0600

    Created QueueTypes class

---
+++ DidactCore/Models/Constants/QueueTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace DidactCore.Models.Constants
+{
+    public static class QueueTypes
+    {
+        public const string StandardQueue = "Standard Queue";
+        public const string FifoQueue = "Fifo Queue";
+    }
+}
