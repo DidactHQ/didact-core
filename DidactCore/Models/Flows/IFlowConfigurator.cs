333bd29fac9d6e2b41a08ec0cd10c893be2b7a97
Daniel Miradakis <dmiradakis@owensgrp.com>
2023-11-19 23:02:17 -0600

    Fixed IFlowConfigurator

---
+++ DidactCore/Models/Flows/IFlowConfigurator.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace DidactCore.Models.Flows
+{
+    public interface IFlowConfigurator
+    {
+        string Name { get; }
+
+        string Description { get; }
+
+        IFlowConfigurator WithName(string name);
+
+        IFlowConfigurator WithDescription(string description);
+
+        Task SaveConfigurationsAsync();
+    }
+}
