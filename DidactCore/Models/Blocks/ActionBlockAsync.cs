1421649d88dc681e42698dd14cb6a994002965d6
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2023-12-10 13:13:24 -0600

    Created ActionBlockAsync class

---
+++ DidactCore/Models/Blocks/ActionBlockAsync.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace DidactCore.Models.Blocks
+{
+    public class ActionBlockAsync
+    {
+        private readonly ILogger<ActionBlockAsync> _logger;
+
+        public Func<Task> Executor { get; private set; }
+        
+        public ActionBlockAsync(ILogger<ActionBlockAsync> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task ExecuteDelegateAsyn()
+        {
+            _logger.LogInformation("Executing delegate...");
+            await Executor.Invoke().ConfigureAwait(false);
+        }
+    }
+}
