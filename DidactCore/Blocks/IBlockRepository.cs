2d1cc41e7de8b232b1028addc20253616ee81594
Daniel Miradakis <dmiradakis@owensgrp.com>
2024-03-05 17:02:40 -0600

    Added SaveLogAsync to IBlockRepository

--- DidactCore/Blocks/IBlockRepository.cs
+++ DidactCore/Blocks/IBlockRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace DidactCore.Blocks
+﻿using System.Threading.Tasks;
+
+namespace DidactCore.Blocks
 {
     public interface IBlockRepository
     {
+        /// <summary>
+        /// Asynchronously saves a Block log to persistent storage.
+        /// </summary>
+        /// <returns></returns>
+        Task SaveLogAsync(string log);
     }
 }
