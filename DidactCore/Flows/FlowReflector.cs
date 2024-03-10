b633ee77adda3a34b13ccb6fc01a88e50f9dfa21
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-03-10 16:04:32 -0500

    Created FlowReflector interface and class

---
+++ DidactCore/Flows/FlowReflector.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace DidactCore.Flows
+{
+    public class FlowReflector : IFlowReflector
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public FlowReflector(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        }
+
+        public IFlow CreateFlow(string flowTypeName)
+        {
+            // Traverse the AppDomain's assemblies to get the type.
+            // Remember that .NET 5+ only has 1 AppDomain going forward, so CurrentDomain is sufficient.
+            var flowType = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes()).Where(t => t.Name == flowTypeName).SingleOrDefault()
+                ?? throw new NullReferenceException();
+
+            // Create an instance of the type using the dependency injection system.
+            // Then safe cast to an IFlow.
+            var flow = ActivatorUtilities.CreateInstance(_serviceProvider, flowType) as IFlow
+                ?? throw new NullReferenceException();
+
+            return flow;
+        }
+    }
+}
---
+++ DidactCore/Flows/IFlowReflector.cs
@@ -0,0 +1,13 @@
+﻿namespace DidactCore.Flows
+{
+    public interface IFlowReflector
+    {
+        /// <summary>
+        /// Retrieves the Flow type from the AppDomain's assemblies using reflection
+        /// and instantiates the Flow using the dependency injection system.
+        /// </summary>
+        /// <param name="flowTypeName"></param>
+        /// <returns></returns>
+        IFlow CreateFlow(string flowTypeName);
+    }
+}
