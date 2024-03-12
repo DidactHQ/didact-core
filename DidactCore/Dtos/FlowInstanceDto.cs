7483e19e0af8b1ce5efc823387c324540a093edb
Daniel Miradakis <dmiradakis@owensgrp.com>
2024-03-12 15:06:34 -0500

    Created FlowInstanceDto

---
+++ DidactCore/Dtos/FlowInstanceDto.cs
@@ -0,0 +1,12 @@
+﻿using DidactCore.Entities;
+using DidactCore.Flows;
+
+namespace DidactCore.Dtos
+{
+    public class FlowInstanceDto
+    {
+        public Flow Flow { get; set; }
+
+        public IFlow FlowInstance { get; set; }
+    }
+}
