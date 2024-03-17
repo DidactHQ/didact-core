159779f3951021a1ae664fbcc61e48739dde51d5
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-03-17 02:21:11 -0500

    DidactInjector wip

---
+++ DidactCore/DependencyInjection/DidactInjector.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DidactCore.DependencyInjection
+{
+    public class DidactInjector
+    {
+        public IServiceCollection ApplicationServiceCollection { get; set; }
+
+        public IServiceCollection InjectorServiceCollection { get; set; }
+
+        public IServiceProvider InjectorServiceProvider { get; set; }
+
+        public DidactInjector(IServiceCollection applicationServiceCollection)
+        {
+            ApplicationServiceCollection = applicationServiceCollection;
+            InjectorServiceCollection = applicationServiceCollection;
+            InjectorServiceProvider = InjectorServiceCollection.BuildServiceProvider();
+        }
+
+        /// <summary>
+        /// Clears the InjectorServiceCollection and resets it to the ApplicationServiceCollection.
+        /// </summary>
+        public void ResetServiceCollection()
+        {
+            InjectorServiceCollection.Clear();
+            InjectorServiceCollection = ApplicationServiceCollection;
+        }
+
+        public void AddServiceCollection(IServiceCollection enhancedServiceCollection)
+        {
+            InjectorServiceCollection = enhancedServiceCollection;
+        }
+
+        /// <summary>
+        /// Builds the InjectorServiceCollection into an IServiceProvider.
+        /// </summary>
+        public void BuildServiceCollection()
+        {
+            InjectorServiceProvider = InjectorServiceCollection.BuildServiceProvider();
+        }
+    }
+}
