9157e482d791421617545e0d65ab52202e218f74
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-03-17 14:01:04 -0500

    Implemented IDidactServiceBridge

---
+++ DidactCore/DependencyInjection/DidactServiceBridge.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DidactCore.DependencyInjection
+{
+    public class DidactServiceBridge : IDidactServiceBridge
+    {
+        public DidactServiceBridge() { }
+
+        public IServiceCollection CreateServiceCollection()
+        {
+            IServiceCollection bridgeServiceCollection = new ServiceCollection();
+
+            // Add services for the Flow Library to the bridgeServiceCollection...
+            // ...
+            // ...
+
+            return bridgeServiceCollection;
+        }
+    }
+}
