ea94aa28d44d6a557f9e593009403361e4d7d93e
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-03-17 14:07:50 -0500

    Added XML docs to IDidactServiceBridge

--- DidactCore/DependencyInjection/IDidactServiceBridge.cs
+++ DidactCore/DependencyInjection/IDidactServiceBridge.cs
@@ -4,6 +4,15 @@ namespace DidactCore.DependencyInjection
 {
     public interface IDidactServiceBridge
     {
+        /// <summary>
+        /// <para>
+        ///     Creates a new <see cref="IServiceCollection"/> containing all dependencies used in the Flow Library.
+        /// </para>
+        /// <para>
+        ///     This <see cref="IServiceCollection"/> is dynamically merged with the <see cref="IServiceCollection"/> from Didact Engine.
+        /// </para>
+        /// </summary>
+        /// <returns></returns>
         IServiceCollection CreateServiceCollection();
     }
 }
