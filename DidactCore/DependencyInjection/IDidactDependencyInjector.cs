4da5929a593f370a732ea396d0bdb7e71c8e823a
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-03-20 00:54:47 -0500

    Added XML docs to IDidactDependencyInjector

--- DidactCore/DependencyInjection/IDidactDependencyInjector.cs
+++ DidactCore/DependencyInjection/IDidactDependencyInjector.cs
@@ -5,10 +5,20 @@ namespace DidactCore.DependencyInjection
 {
     public interface IDidactDependencyInjector
     {
+        /// <summary>
+        /// The original <see cref="IServiceCollection"/> from Didact Engine.
+        /// These services are automatically included in the <see cref="FlowServiceCollection"/>.
+        /// </summary>
         IServiceCollection ApplicationServiceCollection { get; set; }
 
+        /// <summary>
+        /// The <see cref="IServiceCollection"/> that contains all of the services from both Didact Engine and the Flow Library.
+        /// </summary>
         IServiceCollection FlowServiceCollection { get; set; }
 
+        /// <summary>
+        /// The corresponding <see cref="IServiceProvider"/> to the <see cref="FlowServiceCollection"/>.
+        /// </summary>
         IServiceProvider FlowServiceProvider { get; set; }
 
         /// <summary>
