a6e572015a323e9dca4bc57dce54dc6d9ee2c094
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-03-20 00:44:58 -0500

    Created IDidactDependencyInjector

---
+++ DidactCore/DependencyInjection/IDidactDependencyInjector.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DidactCore.DependencyInjection
+{
+    public interface IDidactDependencyInjector
+    {
+        IServiceCollection ApplicationServiceCollection { get; set; }
+
+        IServiceCollection FlowServiceCollection { get; set; }
+
+        IServiceProvider FlowServiceProvider { get; set; }
+
+        void ResetServiceCollection();
+
+        void AddAndRebuildServiceCollection(IServiceCollection bridgeServiceCollection);
+    }
+}
