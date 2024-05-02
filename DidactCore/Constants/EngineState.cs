6edefe8ac0ab07f44ce315785d89fd3a76b0daa5
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-05-02 03:10:49 -0500

    Created EngineState constant class

---
+++ DidactCore/Constants/EngineState.cs
@@ -0,0 +1,8 @@
+﻿namespace DidactCore.Constants
+{
+    public static class EngineState
+    {
+        public const string StartingUp = "Starting up";
+        public const string MountingAssembly = "Mounting assembly";
+    }
+}
