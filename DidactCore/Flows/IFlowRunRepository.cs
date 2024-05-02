115f2c9fcacd8e6063596f53a6b6ef12cde564a9
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-05-02 03:17:11 -0500

    Began IFlowRunRepository

---
+++ DidactCore/Flows/IFlowRunRepository.cs
@@ -0,0 +1,14 @@
+﻿using DidactCore.Entities;
+using System.Threading.Tasks;
+
+namespace DidactCore.Flows
+{
+    public interface IFlowRunRepository
+    {
+        Task<FlowRun> GetFlowRunByIdAsync(long flowRunId);
+
+        Task<FlowRun> CreateAndEnqueueFlowRunAsync(FlowRun flowRun);
+
+        Task<FlowRun> CreateAndExecuteFlowRunAsync(FlowRun flowRun);
+    }
+}
