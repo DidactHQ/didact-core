032514e984dc2b21de627c86e0f947fbc21abb6e
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-05-18 12:32:05 -0500

    Created FlowConfiguratorDto

---
+++ DidactCore/Flows/FlowConfiguratorDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DidactCore.Flows
+{
+    public class FlowConfiguratorDto
+    {
+        public Type FlowType { get; set; } = null!;
+
+        public IFlow? FlowInstance { get; set; }
+
+        public Exception? Exception { get; set; }
+    }
+}
