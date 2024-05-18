152e462f77e11164f937863b315be258159fb5bc
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-05-18 12:21:23 -0500

    EngineState wip

--- DidactCore/Constants/EngineStates.cs
+++ DidactCore/Constants/EngineStates.cs
@@ -3,11 +3,11 @@
     public static class EngineStates
     {
         public const string StartingUp = "Starting up";
-        public const string PausingFlowExecution = "Pausing Flow Execution";
         public const string UnmountingFlowLibrary = "Unmounting Flow Library";
         public const string MountingFlowLibrary = "Mounting Flow Library";
         public const string MountingAssembly = "Mounting assembly";
         public const string ConfiguringFlows = "Configuring Flows";
         public const string ExecutingFlows = "Executing Flows";
+        public const string PausingFlowExecution = "Pausing Flow Execution";
     }
 }
