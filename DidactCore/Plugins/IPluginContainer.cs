33345789d2fdf27b7571f9a2933295951d34ff03
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-05-26 03:31:28 -0500

    Created IPluginContainer and IPluginContainers

---
+++ DidactCore/Plugins/IPluginContainer.cs
@@ -0,0 +1,26 @@
+﻿using DidactCore.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace DidactCore.Plugins
+{
+    public interface IPluginContainer
+    {
+        PluginLoadContext PluginLoadContext { get; set; }
+
+        DateTime PluginLoadedAt { get; set; }
+
+        IDidactDependencyInjector DidactDependencyInjector { get; set; }
+
+        int GetAssemblyCount()
+        {
+            return PluginLoadContext.Assemblies.Count();
+        }
+
+        IPluginContainer SetPluginLoadedAt(DateTime pluginLoadedAt)
+        {
+            PluginLoadedAt = pluginLoadedAt;
+            return this;
+        }
+    }
+}
---
+++ DidactCore/Plugins/IPluginContainers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DidactCore.Plugins
+{
+    public interface IPluginContainers
+    {
+        ICollection<IPluginContainer> PluginContainers { get; set; }
+
+        DateTime PluginContainersLastUpdatedAt { get; set; }
+
+        IPluginContainers SetPluginContainersLastUpdatedAt(DateTime pluginContainersLastUpdatedAt)
+        {
+            PluginContainersLastUpdatedAt = pluginContainersLastUpdatedAt;
+            return this;
+        }
+
+        void FindMatchingPluginContainer(Type type)
+        {
+            var assemblyFullName = type.Assembly.FullName;
+            var matchingPluginContainers = PluginContainers.Select(s => s)
+                .Where(p => p.PluginLoadContext.Assemblies.Select(a => a.FullName).Contains(assemblyFullName))
+                .ToList();
+
+            if (matchingPluginContainers.Count == 0)
+            {
+
+            }
+            if (matchingPluginContainers.Count > 1)
+            {
+
+            }
+        }
+    }
+}
