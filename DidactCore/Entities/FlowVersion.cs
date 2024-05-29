3943f529b2412b0eed7e9ff88d04b238e62caf00
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-05-28 22:33:58 -0500

    Created FlowVersion entity

---
+++ DidactCore/Entities/FlowVersion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DidactCore.Entities
+{
+    public class FlowVersion
+    {
+        public int FlowVersionId { get; set; }
+
+        public long FlowId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string AssemblyVersion { get; set; } = null!;
+
+        public string Version { get; set; } = null!;
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; } = null!;
+
+        public DateTime LastUpdated { get; set; }
+
+        public string LastUpdatedBy { get; set; } = null!;
+
+        public bool Active { get; set; }
+
+        public byte[] RowVersion { get; set; } = null!;
+
+        public virtual Flow Flow { get; set; } = null!;
+
+        public virtual Organization Organization { get; set; } = null!;
+
+        public virtual ICollection<FlowRun> FlowRuns { get; } = new List<FlowRun>();
+    }
+}
