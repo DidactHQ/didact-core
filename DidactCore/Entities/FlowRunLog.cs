dfceec29937fe356b3bf7ed468bbd7f8c6a419eb
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-05-28 23:01:19 -0500

    Created FlowRunLog entity

---
+++ DidactCore/Entities/FlowRunLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DidactCore.Entities
+{
+    public class FlowRunLog
+    {
+        public long FlowRunLogId { get; set; }
+
+        public long FlowRunId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string LogLevel { get; set; } = null!;
+
+        public string Message { get; set; } = null!;
+
+        public DateTime Timestamp { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; } = null!;
+
+        public DateTime LastUpdated { get; set; }
+
+        public string LastUpdatedBy { get; set; } = null!;
+
+        public byte[] RowVersion { get; set; } = null!;
+
+        public virtual FlowRun FlowRun { get; set; } = null!;
+
+        public virtual Organization Organization { get; set; } = null!;
+    }
+}
