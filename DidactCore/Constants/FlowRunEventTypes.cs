63cae9ddcd64da35c0ba6725514accc1d089fdd6
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-10-10 02:52:06 -0500

    Created FlowEventTypes constants

---
+++ DidactCore/Constants/FlowRunEventTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace DidactCore.Constants
+{
+    public static class FlowRunEventTypes
+    {
+        public const string LogEvent = "Log Event";
+        public const string StateChangeEvent = "State Change Event";
+        public const string EngineEvent = "Engine Event";
+        public const string SchedulerEvent = "Scheduler Event";
+        public const string TriggerEvent = "Trigger Event";
+    }
+}
