fd10960c991480a8bfb6c6f425891168d83a0008
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-10-16 02:16:19 -0500

    Created FlowRunEventType entity

---
+++ DidactCore/Entities/FlowRunEventType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DidactCore.Entities
+{
+    internal class FlowRunEventType
+    {
+        public int FlowRunEventTypeId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; } = null!;
+
+        public DateTime LastUpdated { get; set; }
+
+        public string LastUpdatedBy { get; set; } = null!;
+
+        public bool Active { get; set; }
+
+        public byte[] RowVersion { get; set; } = null!;
+    }
+}
