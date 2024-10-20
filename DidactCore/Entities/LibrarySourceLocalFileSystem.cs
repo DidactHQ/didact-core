1805c5780a4574d7c4c9c0d852a4468206470fbc
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-10-19 22:26:28 -0500

    Created LibrarySourceLocalFileSystem entity

---
+++ DidactCore/Entities/LibrarySourceLocalFileSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DidactCore.Entities
+{
+    public class LibrarySourceLocalFileSystem
+    {
+        public long LibrarySourceLocalFileSystemId { get; set; }
+
+        public long LibrarySourceId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string FolderPath { get; set; } = null!;
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; } = null!;
+
+        public DateTime LastUpdated { get; set; }
+
+        public string LastUpdatedBy { get; set; } = null!;
+
+        public bool Active { get; set; }
+
+        public byte[] RowVersion { get; set; } = null!;
+    }
+}
