049d4c680b0222db345186a1f4d53630d347ea7d
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-10-19 22:25:27 -0500

    Created ExecutionVersion entity

---
+++ DidactCore/Entities/ExecutionVersion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DidactCore.Entities
+{
+    public class ExecutionVersion
+    {
+        public long ExecutionVersionId { get; set; }
+
+        public long LibraryVersionId { get; set; }
+
+        public long FlowVersionId { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; } = null!;
+
+        public DateTime LastUpdated { get; set; }
+
+        public string LastUpdatedBy { get; set; } = null!;
+
+        public bool Active { get; set; }
+
+        public byte[] RowVersion { get; set; } = null!;
+    }
+}
