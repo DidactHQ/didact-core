f7370482cbbd14305556b4aa418419862be28191
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-10-19 22:25:49 -0500

    Created Library entity

---
+++ DidactCore/Entities/Library.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DidactCore.Entities
+{
+    public class Library
+    {
+        public long LibraryId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string AssemblyName { get; set; } = null!;
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; } = null!;
+
+        public DateTime LastUpdated { get; set; }
+
+        public string LastUpdatedBy { get; set; } = null!;
+
+        public bool Active { get; set; }
+
+        public byte[] RowVersion { get; set; } = null!;
+    }
+}
