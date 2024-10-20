fffed4fe2451923879180961607744d067cfad4e
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-10-19 22:24:28 -0500

    Created PluginExecutionVersion helper

---
+++ DidactCore/Execution/PluginExecutionVersion.cs
@@ -0,0 +1,13 @@
+﻿namespace DidactCore.Execution
+{
+    public class PluginExecutionVersion
+    {
+        public string FlowTypeName { get; set; } = null!;
+
+        public string FlowVersion { get; set; }
+
+        public string LibraryAssemblyName { get; set; } = null!;
+
+        public string LibraryAssemblyVersion { get; set; }
+    }
+}
