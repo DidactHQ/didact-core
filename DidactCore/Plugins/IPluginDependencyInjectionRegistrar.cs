9741675901f0c8b34116e1c3b0e972427afa5330
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-10-25 02:38:10 -0500

    Created IPluginDependencyInjectionRegistrar types for the flow libraries

---
+++ DidactCore/Plugins/IPluginDependencyInjectionRegistrar.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DidactCore.Plugins
+{
+    public interface IPluginDependencyInjectionRegistrar
+    {
+        /// <summary>
+        /// Creates a new <see cref="IServiceCollection"/> containing all dependencies registered in the plugin.
+        /// </summary>
+        /// <returns></returns>
+        IServiceCollection CreateServiceCollection();
+    }
+}
