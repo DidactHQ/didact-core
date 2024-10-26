9fdbfae37da52d0ef1c9c98a8fd9cafd8c4df5a4
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-10-26 15:54:57 -0500

    Plugin container wip

--- DidactCore/Plugins/IPluginContainer.cs
+++ DidactCore/Plugins/IPluginContainer.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace DidactCore.Plugins
 {
@@ -13,9 +14,9 @@ namespace DidactCore.Plugins
 
         DateTime PluginLoadedAt { get; set; }
 
-        IPluginDependencyInjector PluginDependencyInjector { get; set; }
+        IServiceCollection ApplicationServiceCollection { get; set; }
 
-        int GetAssemblyCount() => PluginAssemblyLoadContext.Assemblies.Count();
+        IPluginDependencyInjector PluginDependencyInjector { get; set; }
 
         /// <summary>
         /// Gets an enumeration of the assemblies from the plugin container's <see cref="DidactCore.Plugins.PluginAssemblyLoadContext"/>.
@@ -23,10 +24,31 @@ namespace DidactCore.Plugins
         /// <returns></returns>
         IEnumerable<Assembly> GetAssemblies() => PluginAssemblyLoadContext.Assemblies;
 
-        IPluginContainer SetPluginLoadedAt(DateTime pluginLoadedAt)
-        {
-            PluginLoadedAt = pluginLoadedAt;
-            return this;
-        }
+        /// <summary>
+        /// Sets a timestamp for when the plugin was loaded into the plugin container.
+        /// </summary>
+        /// <param name="pluginLoadedAt"></param>
+        void SetPluginLoadedAt(DateTime pluginLoadedAt);
+
+        /// <summary>
+        /// Configures the plugin's dependency injection system.
+        /// </summary>
+        void ConfigureDependencyInjection();
+
+        /// <summary>
+        /// Retrieves and instantiates all Flow types to generate their configurators.
+        /// Then uses the configurators and reflection to determine each Flow's <see cref="PluginExecutionVersion"/>.
+        /// </summary>
+        void CollectPluginExecutionVersions();
+
+        /// <summary>
+        /// Retrieves all Flow types from the plugin's assemblies using reflection,
+        /// instantiates each Flow type using the plugin dependency injection system,
+        /// and runs their configuration functions.
+        /// If configuration fails for a specific set of Flows,
+        /// gracefully handles the failures and passes through the successes.
+        /// </summary>
+        /// <returns></returns>
+        Task ConfigureFlowsAsync();
     }
 }
