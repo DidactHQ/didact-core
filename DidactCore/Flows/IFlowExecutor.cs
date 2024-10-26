72b94d3049cffa2d144c70922dda49fddd35fdfc
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-10-26 12:37:43 -0500

    Removed ConfigureFlowsAync from IFlowExecutor

--- DidactCore/Flows/IFlowExecutor.cs
+++ DidactCore/Flows/IFlowExecutor.cs
@@ -13,20 +13,6 @@ namespace DidactCore.Flows
         Task<FlowRunDto> CreateFlowInstanceAsync(FlowRunDto flowRunDto);
 
         /// <summary>
-        /// <para>
-        ///     Retrieves all Flow types from the AppDomain's assemblies using reflection,
-        ///     instantiates each Flow type using the dependency injection system,
-        ///     and runs their configuration functions.
-        /// </para>
-        /// <para>
-        ///     If configuration fails for a specific set of Flows,
-        ///     gracefully handles the failures and passes through the successes.
-        /// </para>
-        /// </summary>
-        /// <returns></returns>
-        Task ConfigureFlowsAsync();
-
-        /// <summary>
         /// Asynchronously executes the IFlow instance by running its ExecuteAsync method and utilizing its Flow metadata.
         /// </summary>
         /// <param name="flowRunDto"></param>
