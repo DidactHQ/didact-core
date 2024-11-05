4a7f5332cc06c632fcfbf582faacdbb1de68be19
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-11-05 00:17:09 -0600

    Added execute, cancellation check, and timeout task methods to FlowExecutor types

--- DidactCore/Flows/FlowExecutor.cs
+++ DidactCore/Flows/FlowExecutor.cs
@@ -1,5 +1,6 @@
 ﻿using DidactCore.Engine;
 using DidactCore.Plugins;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
 using System.Threading;
@@ -10,14 +11,16 @@ namespace DidactCore.Flows
     public class FlowExecutor : IFlowExecutor
     {
         private readonly IEngineSupervisor _engineSupervisor;
-        private readonly IFlowRepository _flowRepository;
-        private readonly IFlowLogger _flowLogger;
+        private readonly IFlowRunRepository _flowRunRepository;
+        private readonly ILogger<IFlowExecutor> _logger;
+        private readonly int _flowRunCancellationCheckInterval;
 
-        public FlowExecutor(IEngineSupervisor engineSupervisor, IFlowRepository flowRepository, IFlowLogger flowLogger)
+        public FlowExecutor(ILogger<IFlowExecutor> logger, IEngineSupervisor engineSupervisor, IFlowRunRepository flowRunRepository)
         {
             _engineSupervisor = engineSupervisor ?? throw new ArgumentNullException(nameof(engineSupervisor));
-            _flowRepository = flowRepository ?? throw new ArgumentNullException(nameof(flowRepository));
-            _flowLogger = flowLogger ?? throw new ArgumentNullException(nameof(flowLogger));
+            _flowRunRepository = flowRunRepository ?? throw new ArgumentNullException(nameof(flowRunRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _flowRunCancellationCheckInterval = engineSupervisor.EngineTuning.FlowRunCancellationCheckInterval;
         }
 
         public async Task<FlowRunDto> FetchFlowRunAsync()
@@ -44,7 +47,7 @@ namespace DidactCore.Flows
             // Remember that .NET 5+ only has 1 AppDomain going forward, so CurrentDomain is sufficient.
             var flowType = pluginContainer.GetAssemblies()
                 .SelectMany(s => s.GetTypes())
-                .Where(t => t.Name == flowRunDto.Flow.TypeName && t.GetInterfaces().Contains(typeof(IFlow)) && t.IsClass && !t.IsAbstract)
+                .Where(t => t.Name == flowRunDto.FlowTypeName && t.GetInterfaces().Contains(typeof(IFlow)) && t.IsClass && !t.IsAbstract)
                 .SingleOrDefault();
 
             if (flowType is null)
@@ -64,19 +67,46 @@ namespace DidactCore.Flows
             return flowRunDto;
         }
 
-        public async Task ExecuteFlowRunAsync(FlowRunDto flowRunDto)
+        public async Task CheckForFlowRunCancellationAsync(long flowRunId, CancellationToken compositeCancellationToken)
+        {
+            while (true)
+            {
+                try
+                {
+                    var isCancelled = await _flowRunRepository.CheckIfFlowRunIsCancelledAsync(flowRunId);
+                    if (isCancelled)
+                        return;
+                    else
+                        await Task.Delay(_flowRunCancellationCheckInterval, compositeCancellationToken);
+                }
+                catch (Exception)
+                {
+                    _logger.LogError("FlowRun cancellation check failed for FlowRunId {flowRunId}.", flowRunId);
+                }
+            }
+        }
+
+        public async Task ExecuteFlowRunTimeoutCountdownAsync(int flowRunTimeoutSeconds, CancellationToken compositeCancellationToken)
+        {
+            var flowRunTimeoutMilliseconds = flowRunTimeoutSeconds * 1000;
+            await Task.Delay(flowRunTimeoutMilliseconds, compositeCancellationToken);
+        } 
+
+        public async Task ExecuteFlowRunAsync(FlowRunDto flowRunDto, CancellationToken compositeCancellationToken)
         {
             if (flowRunDto.FlowInstance is null)
             {
                 throw new Exception("Oops.");
             }
 
-            // TODO Implement FlowRun cancellationtoken.
-            using var flowRunCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(_engineSupervisor.CancellationToken);
-            var flowExecutionContext = new FlowExecutionContext(flowRunDto.FlowRun.JsonPayload, flowRunCancellationTokenSource.Token);
+            var flowExecutionContext = new FlowExecutionContext(flowRunDto.StringifiedJsonPayload, compositeCancellationToken);
             try
             {
                 await flowRunDto.FlowInstance.ExecuteAsync(flowExecutionContext);
+            }
+            catch (OperationCanceledException)
+            {
+                await _flowRunRepository.CancelFlowRunAsync(flowRunDto.FlowRunId);
             }
             catch (Exception)
             {
@@ -84,6 +114,35 @@ namespace DidactCore.Flows
                 // Handle retries here.
                 // After final failure, log to storage.
                 // Do not actually throw;
+            }
+        }
+
+        public async Task ExecuteIntelligentAsync(FlowRunDto flowRunDto)
+        {
+            // Create cancellation token for FlowRun cancellations from persistent storage.
+            using var flowRunCancellationTokenSource = new CancellationTokenSource();
+            // Create cancellation token for FlowRun timeout.
+            using var flowRunTimeoutCancellationTokenSource = new CancellationTokenSource();
+            // Create a composite cancellation token for engine shutdown, FlowRun cancellation, and FlowRun timeout.
+            using var compositeCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(flowRunCancellationTokenSource.Token, flowRunTimeoutCancellationTokenSource.Token, _engineSupervisor.CancellationToken);
+            var compositeCancellationToken = compositeCancellationTokenSource.Token;
+            
+            var flowRunExecutionTask = ExecuteFlowRunAsync(flowRunDto, compositeCancellationToken);
+            var flowRunTimeoutTask = ExecuteFlowRunTimeoutCountdownAsync(flowRunDto.TimeoutSeconds, compositeCancellationToken);
+            var flowRunCancellationCheckTask = CheckForFlowRunCancellationAsync(flowRunDto.FlowRunId, compositeCancellationToken);
+
+            var completedTask = await Task.WhenAny(flowRunExecutionTask, flowRunCancellationCheckTask, flowRunTimeoutTask);
+            if (completedTask == flowRunCancellationCheckTask)
+            {
+                // TODO Handle cancellation reason.
+                flowRunCancellationTokenSource?.Cancel();
+                await flowRunExecutionTask;
+            }
+            else if (completedTask == flowRunTimeoutTask)
+            {
+                // TODO Handle timeout cancellation reason.
+                flowRunTimeoutCancellationTokenSource?.Cancel();
+                await flowRunExecutionTask;
             }
         }
     }
--- DidactCore/Flows/IFlowExecutor.cs
+++ DidactCore/Flows/IFlowExecutor.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace DidactCore.Flows
 {
@@ -19,10 +20,35 @@ namespace DidactCore.Flows
         Task<FlowRunDto> CreateFlowInstanceAsync(FlowRunDto flowRunDto);
 
         /// <summary>
-        /// Asynchronously executes the FlowRun by running its <see cref="IFlow.ExecuteAsync(IFlowExecutionContext)"/> method.
+        /// Asynchronously checks if the FlowRun was cancelled from persistent storage in a loop.
+        /// </summary>
+        /// <param name="flowRunId"></param>
+        /// <param name="compositeCancellationToken"></param>
+        /// <returns></returns>
+        Task CheckForFlowRunCancellationAsync(long flowRunId, CancellationToken compositeCancellationToken);
+
+        /// <summary>
+        /// Asynchronously executes a <see cref="Task"/> countdown whose completion represents when the FlowRun is supposed to timeout.
+        /// </summary>
+        /// <param name="flowRunTimeoutSeconds"></param>
+        /// <param name="compositeCancellationToken"></param>
+        /// <returns></returns>
+        Task ExecuteFlowRunTimeoutCountdownAsync(int flowRunTimeoutSeconds, CancellationToken compositeCancellationToken);
+
+        /// <summary>
+        /// Asynchronously executes the FlowRun by running its <see cref="IFlow.ExecuteAsync"/> method.
+        /// </summary>
+        /// <param name="flowRunDto"></param>
+        /// <param name="compositeCancellationToken"></param>
+        /// <returns></returns>
+        Task ExecuteFlowRunAsync(FlowRunDto flowRunDto, CancellationToken compositeCancellationToken);
+
+        /// <summary>
+        /// Asynchronously executes the FlowRun and initializes its cancellation checks and countdown <see cref="Task"/>.
+        /// Intelligently handles cancellation from an engine shutdown, FlowRun cancellation, or FlowRun timeout.
         /// </summary>
         /// <param name="flowRunDto"></param>
         /// <returns></returns>
-        Task ExecuteFlowRunAsync(FlowRunDto flowRunDto);
+        Task ExecuteIntelligentAsync(FlowRunDto flowRunDto);
     }
 }
