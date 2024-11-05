c336a9aa84aa248763d0cf7e16815c42112bd008
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-11-05 00:15:55 -0600

    FlowRunDto wip

--- DidactCore/Flows/FlowRunDto.cs
+++ DidactCore/Flows/FlowRunDto.cs
@@ -5,6 +5,14 @@ namespace DidactCore.Flows
 {
     public class FlowRunDto
     {
+        public long FlowRunId { get; set; }
+
+        public int TimeoutSeconds { get; set; }
+
+        public string FlowTypeName { get; set; } = null!;
+
+        public string? StringifiedJsonPayload { get; set; }
+
         public FlowRun FlowRun { get; set; } = null!;
 
         public PluginExecutionVersion PluginExecutionVersion { get; set; } = null!;
