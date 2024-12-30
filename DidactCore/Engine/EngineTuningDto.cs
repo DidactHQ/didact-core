3e11e7aca1b7c48221982019a2fc75fbcf6c9dc0
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2024-12-30 00:00:18 -0600

    EngineTuningDto

--- DidactCore/Engine/EngineTuningDto.cs
+++ DidactCore/Engine/EngineTuningDto.cs
@@ -3,5 +3,12 @@
     public class EngineTuningDto
     {
         // TODO Implement
+        public decimal ThreadFactor { get; set; }
+
+        public decimal TaskFactor { get; set; }
+
+        public int FlowRunCancellationCheckInterval { get; set; }
+
+        public int EngineShutdownCheckInterval { get; set; }
     }
 }
