010d71c5b63460e8e0c284d86c444ca5e670ef20
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-04-05 16:04:08 -0500

    Created DidactDefaults constants class

---
+++ DidactCore/Constants/DidactDefaults.cs
@@ -0,0 +1,8 @@
+﻿namespace DidactCore.Constants
+{
+    public static class DidactDefaults
+    {
+        public const string DefaultFlowVersion = "v1.0.0";
+        public const string DefaultQueueName = "Default";
+    }
+}
