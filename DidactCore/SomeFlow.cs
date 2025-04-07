5ca83dda242f334f94651ba02409ef8c3fe7f812
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-04-07 01:59:24 -0500

    SomeFlow wip

--- DidactCore/SomeFlow.cs
+++ DidactCore/SomeFlow.cs
@@ -1,6 +1,4 @@
-﻿using DidactCore.Blocks;
-using DidactCore.Constants;
-using DidactCore.Flows;
+﻿using DidactCore.Flows;
 using System.Threading.Tasks;
 
 namespace DidactCore
@@ -8,32 +6,26 @@ namespace DidactCore
     public class SomeFlow : IFlow
     {
         private readonly IFlowLogger _flowLogger;
-        private readonly ActionBlock<string> _block1;
-        private readonly ActionBlock<int> _block2;
 
-        public SomeFlow(IFlowLogger flowLogger, IBlockFactory blockFactory)
+        public SomeFlow(IFlowLogger flowLogger)
         {
             _flowLogger = flowLogger;
-            _block1 = blockFactory.CreateActionBlock<string>();
-            _block2 = blockFactory.CreateActionBlock<int>();
         }
 
-        public async Task<IFlowConfigurator> ConfigureAsync(IFlowConfigurator flowConfigurator)
+        public Task<IFlowConfigurator> ConfigureAsync(IFlowConfigurator flowConfigurator)
         {
-            await Task.CompletedTask;
-            return flowConfigurator
+            flowConfigurator
                 .WithTypeName(GetType().Name)
-                .WithName("SomeFlow Custom Name")
-                .WithDescription("A flow description.")
-                .AsVersion("1.0-alpha")
-                .ForQueue(QueueTypes.HyperQueue, "Default");
+                .WithDescription("A sample flow");
+
+            return Task.FromResult(flowConfigurator);
         }
 
         public async Task ExecuteAsync(IFlowExecutionContext context)
         {
-            await _block1.ExecuteAsync();
-            await _block2 .ExecuteAsync();
-            await Task.CompletedTask;
+            _flowLogger.LogInformation("Starting work...");
+            await Task.Delay(1000);
+            _flowLogger.LogInformation("Work completed.");
         }
     }
 }
