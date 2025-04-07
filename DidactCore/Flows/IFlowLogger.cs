a3da3b376287957554feff3fa326fc91e72a2def
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-04-07 01:47:46 -0500

    IFlowLogger wip

--- DidactCore/Flows/IFlowLogger.cs
+++ DidactCore/Flows/IFlowLogger.cs
@@ -1,6 +1,11 @@
-﻿namespace DidactCore.Flows
+﻿using System;
+
+namespace DidactCore.Flows
 {
     public interface IFlowLogger
     {
+        void LogInformation(string message, params object[] args);
+
+        void LogError(Exception ex, string message, params object[] args);
     }
 }
