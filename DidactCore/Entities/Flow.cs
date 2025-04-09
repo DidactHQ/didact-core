a34596b80edf39f1906a0bee5a850c7de1288000
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-04-09 01:31:37 -0500

    Made Name prop nullable in Flow entity

--- DidactCore/Entities/Flow.cs
+++ DidactCore/Entities/Flow.cs
@@ -11,7 +11,7 @@ namespace DidactCore.Entities
 
         public long LibraryId { get; set; }
 
-        public string Name { get; set; } = null!;
+        public string? Name { get; set; }
 
         public string? Description { get; set; }
 
